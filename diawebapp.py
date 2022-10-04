09ee0bbfb765821b2056e12aae69f27b56d56c30
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-05 01:45:36 +0530

    Update diawebapp.py

--- diawebapp.py
+++ diawebapp.py
@@ -62,7 +62,7 @@ def main():
     
     #getting the iput data from the uder
     
-    <font color=‘red’>Pregnancies</font>, unsafe_allow_html=True)= st.text_input('Number of Pregnancies')
+    <font color=‘red’>Pregnancies</font>= st.text_input('Number of Pregnancies')
     Glucose = st.text_input('Glucose Level')
     BloodPressure = st.text_input('Blood Pressure value')
     SkinThickness = st.text_input('SkinThickness value')
