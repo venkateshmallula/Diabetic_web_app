febcc5ab28ccc68a8bc455d4751c2f52429e1226
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-05 02:23:14 +0530

    Update diawebapp.py

--- diawebapp.py
+++ diawebapp.py
@@ -30,20 +30,26 @@ def diabetes_prediction(input_data):
     else:
       return('The person is diabetic')
 
-def add_bg_from_url():
+from PIL import Image
+img = Image.open("diabetes.jpg")
+st.image(img)
+
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://images.pexels.com/photos/6941105/pexels-photo-6941105.jpeg?auto=compress&cs=tinysrgb&w=600");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-add_bg_from_url()
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('diabetes.jpg')
     
 
 #steamlit library code
