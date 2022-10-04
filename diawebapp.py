ed86f965418fae7541708671a5141154c040ef8e
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-05 02:09:38 +0530

    Update diawebapp.py

--- diawebapp.py
+++ diawebapp.py
@@ -46,16 +46,17 @@ def add_bg_from_local(image_file):
     unsafe_allow_html=True
     )
 add_bg_from_local('diabetes.jpg')
+
+from PIL import Image
+    
+    img = Image.open("https://www.pexels.com/photo/landscape-nature-sky-man-6620743/")
+    st.image(img)
  
     
 
 #steamlit library code
 def main():
     
-    from PIL import Image
-    
-    img = Image.open("https://www.pexels.com/photo/landscape-nature-sky-man-6620743/")
-    st.image(img)
     #giving the title
     st.title('DIABETES PREDICTION WEB APP')
     
