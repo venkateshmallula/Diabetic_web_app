6b316c261cf19c4c1236b9276dd80ed4f4764f2d
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-05 01:23:56 +0530

    Update diawebapp.py

--- diawebapp.py
+++ diawebapp.py
@@ -29,6 +29,22 @@ def diabetes_prediction(input_data):
       return('The person is not diabetic')
     else:
       return('The person is diabetic')
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
  
     
 
