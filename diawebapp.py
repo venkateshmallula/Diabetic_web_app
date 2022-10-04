2593ecdc7fdbe9db054c4b883ada6cc8aebd04d0
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-05 02:06:23 +0530

    Update diawebapp.py

--- diawebapp.py
+++ diawebapp.py
@@ -52,11 +52,11 @@ add_bg_from_local('diabetes.jpg')
 #steamlit library code
 def main():
     
-    #from PIL import Image
+    from PIL import Image
     
-    #img = Image.open("https://www.pexels.com/photo/landscape-nature-sky-man-6620743/")
-    #st.image(img)
-    #giving the title
+    img = Image.open("https://www.pexels.com/photo/landscape-nature-sky-man-6620743/")
+    st.image(img)
+    giving the title
     st.title('DIABETES PREDICTION WEB APP')
     
     #getting the iput data from the uder
